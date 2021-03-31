ca3cacf355eaafdda0beb3b1f117b987bb430813
Collin Winner <cmwinner89@gmail.com>
2021-03-31 16:27:01 -0400

    complete first project:

--- First_Routes_Controllers/app/controllers/users_controller.rb
+++ First_Routes_Controllers/app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        user = User.all
+        render json: user
+    end
+
+    def user_params
+        params.require(:users).permit(:name, :email)
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to user_url(user)
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+
+        user.delete
     end
 end
